44d66e4e3db997c18c35601323025e40922f8d99
Victor Mours <victor.mours@gmail.com>
2016-07-19 09:57:32 +0200

    Require jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,3 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require_tree ./channels
+//= require_tree .
